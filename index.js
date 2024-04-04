f1e52b5152852cb9cd818c8e86ee100b82b2e99b
michaela-wasson <108202135+michaela-wasson@users.noreply.github.com>
2024-04-03 19:47:05 -0700

    adds express and morgan

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+morgan= require('morgan');
+fs =require('fs');
+path = require('path');
+const app = express();
+const accessLogStream = fs.createWriteStream(path.join(_dirname, 'log.txt'), {flags: 'a'});
+
+app.use(morgan('common'));
+app.use(morgan ('combined', {stream:accessLogStream}));
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
+
+app.get('/movies', (req, res) => {
+    res.json(topTenMovies);
+});
+
+app.get('/', (req, res) => {
+    res.send('Let me get you started');
+});
+
+app.get('/documentation', express.static('public'));
+
+
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});

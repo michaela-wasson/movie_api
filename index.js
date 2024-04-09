319f8ad81cf76b26b165f27c83a51716a3281a79
michaela-wasson <108202135+michaela-wasson@users.noreply.github.com>
2024-04-08 18:58:13 -0700

    wrote a path to /documentation with express

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ morgan= require('morgan');
 fs =require('fs');
 path = require('path');
 const app = express();
-//const accessLogStream = fs.createWriteStream(path.join(_dirname, 'log.txt'), {flags: 'a'});
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'});
 
 app.use(morgan('common'));
-//app.use(morgan ('combined', {stream:accessLogStream}));
+app.use(morgan ('combined', {stream:accessLogStream}));
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
   });
+app.use("/documentation", express.static("public"));
 
 
 app.get('/movies', (req, res) => {
+    //const topTenMovies = {"one" : [15, 4.5],
+        //"two" : [34, 3.3],
+        //"three" : [67, 5.0],
+        //"four" : [32, 4.1]};
+    //var topTenMovies = JSON.stringify(dict);
+    //fs.writeFile("toptenmovies.json", topTenMovies);
+    //I'm guessing the previous code isn't supposed to go here
     res.json(topTenMovies);
 });
 
@@ -21,9 +29,16 @@ app.get('/', (req, res) => {
     res.send('Let me get you started');
 });
 
-app.get('/documentation', express.static('public'));
+app.get("/documentation", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "documentation.html"));
+});
 
 
-//app.listen(8080, () => {
-    //console.log('Your app is listening on port 8080.');
-//});
+
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
+
+
+
+

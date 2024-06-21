41cd5b586043445bbbced374f3f968eacd30532d
michaela-wasson <108202135+michaela-wasson@users.noreply.github.com>
2024-06-20 22:22:25 -0700

    modified index mongoose connect

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const Movies = Models.Movie;
 const Users = Models.User;
 //mongoose.connect('mongodb://localhost:27017/[myflixdb]', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect(process.env.CONNECTION_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  dbName: "myflixdb"
 });
 
 const express = require('express');
